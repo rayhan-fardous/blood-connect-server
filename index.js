6aa9f9abed5727cf426db08cc06318ab4ee84e7b
Md Rayhan ul Fardous <49313477+rayhan-fardous@users.noreply.github.com>
2026-06-21 11:49:51 +0600

    feat: add wrappy function to preserve callback properties

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+
+dotenv.config(); 
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.MONGODB_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // await client.connect();
+    // await client.db('admin').command({ ping: 1 });
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('BloodConnect backend server is running');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
